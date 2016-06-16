20fc2da814c25bb716ae914a77b714c7dfd906ab
Saturn226 <serenehopes@gmail.com>
2016-06-15 23:22:34 -0400

    signup isnt working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,34 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get "/" do
     erb :index
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    #@user = User.new(username: params[:username], 
+     # password: params[:password], first_name: params[:first_name], last_name: params[:last_name],
+      #city: params[:city], state: params[:state], email: params[:email])
+    @user = User.new(username: params[:username], password: params[:password])
+    if @user.save
+      raise "success"
+    else
+      raise "failure"
+    end
+    #erb :"users/account"
+    #redirect '/account'
+  end
+
+  get '/account' do
+    erb :"users/account"
   end
 
 end
