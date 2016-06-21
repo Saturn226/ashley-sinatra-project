2abe0e0bc40d67a89e7ab7660d8f2b1f01da0ad4
Saturn226 <serenehopes@gmail.com>
2016-06-20 23:14:35 -0400

    80 percent functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,34 +21,53 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:id] = user.id
-      redirect '/account'
+      redirect "/#{user.username}/account"
     else
       raise 'failure'
     end
 
-    redirect '/account'
+    redirect "/#{user.username}/account"
   end
 
   post "/signup" do
 
-    @user = User.new(params)
-    if @user.save
+    user = User.new(params[:user])
+    if user.save
      #raise @user.inspect
-      redirect '/account'
+      redirect "/#{user.username}/account"
     else
       raise "failure"
     end
 
   end
 
-  get '/account' do
-    @user = User.find_by_id(session[:id])
-    erb :"users/account"
+  get '/:username/account' do
+    if logged_in?
+      @user = User.find_by_id(session[:id])
+      erb :"users/account"
+    else
+      redirect '/'
+    end
   end
 
   post '/logout' do
     session.clear
     redirect '/'
+  end
+
+  get "/account" do
+    user = User.find_by_id(session[:id])
+    redirect "/#{user.username}/account"
+  end
+
+  helpers do
+      def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616000649) do
+ActiveRecord::Schema.define(version: 20160620225749) do
 
   create_table "pets", force: :cascade do |t|
     t.string  "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160616000649) do
     t.string  "bio"
     t.string  "price"
     t.boolean "adoptable"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
